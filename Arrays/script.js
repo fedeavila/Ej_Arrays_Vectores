35b28a1206e1146fb3a401a6d1a82c6db72de8f1
Federico Avila <fedeavila@outlook.com>
2023-04-14 01:19:57 -0300

    x

--- Arrays/script.js
+++ Arrays/script.js
@@ -84,3 +84,41 @@ console.log(cosas.sort());
 // Acceso al ultimo elemento del Array
 let ultimoElementoDelArray = cosas[cosas.length - 1];
 
+// Metodo Push() - Para agregar un elemento al final del Array
+let nombres = ["Marcelo", "Rodrigo", "Emilia", "Martin"];
+console.log(nombres);
+let retorno = nombres.push("Federico");
+console.log(retorno);
+console.log(nombres);
+
+// Metodo Pop() - Para quitar el ultimo elemento.
+nombres.pop(nombres);
+console.log(nombres);
+
+// Metodo Unshift() - Para agregar elem al principio.
+nombres.unshift("Valentinga");
+console.log(nombres);
+
+// Metodo shift() - Para eliminar primer elemento.
+nombres.unshift("Valentinga");
+console.log(nombres);
+
+
+// Splice()
+nombres.splice(1, 2) // A partir del inciden 1, borrame 2 elementos.
+console.log(nombres);
+
+// Concat()
+let sectorA = ["alex", "pepe"];
+let sectorB = ["maria", "valentina"];
+
+let sectorAB = sectorA.concat(sectorB);
+console.log(sectorAB);
+
+// Slice()
+let chicas = sectorAB.splice(2);
+console.log(chicas);
+
+let chicos = sectorAB.splice(0, 2);
+console.log(chicos);
+
